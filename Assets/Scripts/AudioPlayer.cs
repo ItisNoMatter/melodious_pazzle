5320bfc40aa31856b3b5c1bcf222d3e4f2e85f65
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-08-21 15:24:35 +0900

    2019.4.29f1でプロジェクトごと再構成

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField]AudioClip audioclip;
+    AudioSource audiosource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            audiosource.PlayOneShot(audioclip);
+            Debug.Log("“–‚½‚è‚Ü‚µ‚½");
+        }
+    }
+}
