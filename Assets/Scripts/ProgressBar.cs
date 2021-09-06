2d1a1946c3a5607b54a020d90bd432ee6fe8aaa8
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-09-06 21:23:49 +0900

    進行バーの実装/HPゲージの表示位置の調整

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressBar : MonoBehaviour
+{
+    private float currentTime;
+    
+    // 1ステージの時間を1m30sで固定
+    private float maxTime = 90f;
+
+    public Slider slider;
+    void Start()
+    {
+        slider.value = 1;
+        currentTime = 0;
+        slider.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        slider.value = (float)currentTime / (float)maxTime;
+        Debug.Log(currentTime);
+    }
+
+}
