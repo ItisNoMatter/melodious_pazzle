9c53a9860133c3e027b98d026770f31c79b65982
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-09-08 16:58:03 +0900

    ステージセレクト処理を修正

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageSelect : MonoBehaviour
 {
-    public void OnPressedStageButton(int stage)
+    public GameObject StageText;
+
+    public void LoadPressedButton(int stage)
     {
+        Text stageText = StageText.GetComponent<Text>();
+        stageText.color = Color.yellow;
+        StartCoroutine(OnPressedStageButton(1));
+        // Debug.Log(stage);
+    }
+
+    public IEnumerator OnPressedStageButton(int stage)
+    {
+
         if (stage == 1)
         {
             SceneManager.LoadScene("Scenes/MainScene");
         }
 
+        else if (stage == 2)
+        {
+            // 未実装
+            // ヒープ音など鳴らせるとよい？
+        }
+
+        // 1秒待つ
+        yield return new WaitForSeconds(1.0f);
     }
 }
