97c61b4ec44bb8895336fbd1b6904c7de6b00a8a
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-09-12 00:08:45 +0900

    スコアと操作説明シーンの実装

---
+++ Assets/Scripts/OpenGuidMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenGuidMenu : MonoBehaviour
+{
+    bool onPressedXkey = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("X");
+            SceneManager.LoadScene("Scenes/Guid");
+            onPressedXkey = true;
+        }
+        if (onPressedXkey==false)
+        {
+            SceneManager.LoadScene("Scenes/Title");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("ゲームを中断します");
+            SceneManager.LoadScene("Scenes/Title");
+        }
+    }
+}
